2d541822d0ec42c184acf2e1c2a31b7437aaa84d
Kevin Galligan <galligankevinp@gmail.com>
2016-04-12 10:51:00 +0100

    part 1

--- assignment3/OpenGL_3D/OpenGL_3D/Form1.h
+++ assignment3/OpenGL_3D/OpenGL_3D/Form1.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "OpenGL_WinApp.h"
+
 namespace OpenGL_3D {
 
 	using namespace System;
@@ -8,19 +10,22 @@ namespace OpenGL_3D {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace OpenGLForm;
 
 	/// <summary>
 	/// Summary for Form1
 	/// </summary>
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
+	private: OpenGLForm::Create_OpenGL ^OpenGL;
 	public:
 		Form1(void)
 		{
 			InitializeComponent();
 			//
 			//TODO: Add the constructor code here
-			//
+			// Create an OpenGL frame on the form (xpos, ypos, width, height).
+			OpenGL = gcnew Create_OpenGL(this, 10, 10, 640, 480);
 		}
 
 	protected:
@@ -34,12 +39,21 @@ namespace OpenGL_3D {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::TrackBar^  trackBar1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TrackBar^  trackBar2;
+	private: System::Windows::Forms::TrackBar^  trackBar3;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	protected: 
+	private: System::ComponentModel::IContainer^  components;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,19 +62,120 @@ namespace OpenGL_3D {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->trackBar2 = (gcnew System::Windows::Forms::TrackBar());
+			this->trackBar3 = (gcnew System::Windows::Forms::TrackBar());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar3))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Interval = 10;
+			this->timer1->Tick += gcnew System::EventHandler(this, &Form1::timer1_Tick);
+			// 
+			// trackBar1
+			// 
+			this->trackBar1->Location = System::Drawing::Point(24, 520);
+			this->trackBar1->Maximum = 180;
+			this->trackBar1->Minimum = -180;
+			this->trackBar1->Name = L"trackBar1";
+			this->trackBar1->Size = System::Drawing::Size(600, 45);
+			this->trackBar1->TabIndex = 0;
+			this->trackBar1->Scroll += gcnew System::EventHandler(this, &Form1::trackBar1_Scroll);
+			this->trackBar1->ValueChanged += gcnew System::EventHandler(this, &Form1::trackBar1_ValueChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(663, 520);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"label1";
+			// 
+			// trackBar2
+			// 
+			this->trackBar2->Location = System::Drawing::Point(772, 56);
+			this->trackBar2->Maximum = 50;
+			this->trackBar2->Minimum = 1;
+			this->trackBar2->Name = L"trackBar2";
+			this->trackBar2->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBar2->Size = System::Drawing::Size(45, 278);
+			this->trackBar2->TabIndex = 2;
+			this->trackBar2->Value = 10;
+			this->trackBar2->Scroll += gcnew System::EventHandler(this, &Form1::trackBar2_Scroll);
+			// 
+			// trackBar3
+			// 
+			this->trackBar3->Location = System::Drawing::Point(861, 56);
+			this->trackBar3->Maximum = 180;
+			this->trackBar3->Minimum = -180;
+			this->trackBar3->Name = L"trackBar3";
+			this->trackBar3->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBar3->Size = System::Drawing::Size(45, 278);
+			this->trackBar3->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(769, 352);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(40, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Radius";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(858, 352);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(45, 13);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"Latitude";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(984, 602);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->trackBar3);
+			this->Controls->Add(this->trackBar2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->trackBar1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar3))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e)
+			 {
+				 //OpenGL->longitude += 0.025;
+				 OpenGL->Render();
+			 }
+	private: System::Void trackBar1_Scroll(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void trackBar1_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+				 OpenGL->longitude = (trackBar1->Value) * M_PI / 180.0;
+				 // Dodge to move focus from trackbar (also displays value)
+				 label1->Text = "Longitude: " + System::Convert::ToString(trackBar1->Value);
+			 }
+	private: System::Void trackBar2_Scroll(System::Object^  sender, System::EventArgs^  e) {
+			 }
+};
 }
 
